e7ae5ad3d2d7de7e1089217400eac651270de7c1
Lucas Witthaus <lucas.witthaus@udo.edu>
2017-10-26 15:26:01 +0200

    Curve fit

--- V206-Waermepumpe/Temperaturen.py
+++ V206-Waermepumpe/Temperaturen.py
@@ -1,11 +1,26 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.optimize import curve_fit
 
 n, T1, T2, pa, pb, p = np.genfromtxt('daten.txt', unpack=True)
-plt.plot(n*60,T1, 'r.', label='T1')
-plt.plot(n*60,T2, 'b.', label='T2')
+
+def f(x, a, b, c):
+   return a * x**2 + b*x + c
+
+x_plot = np.linspace(0,2000)
+params, covariance_matrix = curve_fit(f, n*60, T1)
+plt.plot(x_plot, f(x_plot, *params), 'k-', label='Fit', linewidth=0.5)
+
+params, covariance_matrix = curve_fit(f, n*60, T2)
+plt.plot(x_plot, f(x_plot, *params), 'k-', label='Fit', linewidth=0.5)
+
+plt.plot(n*60,T1, 'r.', label='T1', Markersize=4)
+plt.plot(n*60,T2, 'b.', label='T2', Markersize=4)
 plt.title('Temperaturen')
 plt.legend()
+plt.grid()
 plt.xlabel('t / s')
 plt.ylabel('T / °C')
+
+
 plt.savefig('build/Temperaturen.pdf')
