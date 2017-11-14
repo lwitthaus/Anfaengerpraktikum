5ffc84771ca43846d9cf89770c6e72ef031177c6
Lucas Witthaus <lucas.witthaus@udo.edu>
2017-11-14 15:21:18 +0100

    Temperaturen Graph fix

--- V206-Waermepumpe/Temperaturen.py
+++ V206-Waermepumpe/Temperaturen.py
@@ -8,21 +8,21 @@ def f(x, a, b, c):
    return a * x**2 + b*x + c
 
 x_plot = np.linspace(0,2000)
-params, covariance_matrix = curve_fit(f, n*60, T1)
+params, covariance_matrix = curve_fit(f, n*60, T1+273.15)
 plt.plot(x_plot, f(x_plot, *params), 'r-', label='Anpassungsfunktion T1', linewidth=0.5)
 print(params)
 print(np.sqrt(np.diag(covariance_matrix)))
 
-params, covariance_matrix = curve_fit(f, n*60, T2)
+params, covariance_matrix = curve_fit(f, n*60, T2+273.15)
 plt.plot(x_plot, f(x_plot, *params), 'b-', label='Anpassungsfunktion T2', linewidth=0.5)
 
-plt.plot(n*60,T1, 'r.', label='T1', Markersize=4)
-plt.plot(n*60,T2, 'b.', label='T2', Markersize=4)
+plt.plot(n*60,T1+273.15, 'r.', label='T1', Markersize=4)
+plt.plot(n*60,T2+273.15, 'b.', label='T2', Markersize=4)
 plt.title('Temperaturen')
 plt.legend()
 plt.grid()
 plt.xlabel('t / s')
-plt.ylabel('T / °C')
+plt.ylabel('T / K')
 print(params)
 print(np.sqrt(np.diag(covariance_matrix)))
 
