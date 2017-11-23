e4d1652a66e0a1cdf17c2850130c610f302002df
Lucas Witthaus <lucas.witthaus@udo.edu>
2017-11-23 13:21:19 +0100

    polynom

--- V103-Biegung/polynom.py
+++ V103-Biegung/polynom.py
@@ -6,4 +6,9 @@ x = np.array([40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240,260, 280, 300, 
 L = 0.5
 print(((L * (x*10**(-3))**2) - ((x * 10**(-3))**(3) * 1/3)) * 10**3)
 
+print('----------------------------')
 #quadratischer Stab einseitig
+
+#runder Stab beidseitig
+n, x, D = np.genfromtxt('beidseitig1.txt', unpack=True)
+print((3*0.554**2*x*10**(-2) - 4*(x*10**(-2))**3)*10**3)
