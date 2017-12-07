7510a83b25dcdbdf07e08029fdabbaa277f791b7
Christopher Krause <christopher2.krause@tu-dortmund.de>
2017-12-07 14:39:39 +0100

    Auswertung

--- V353-Relaxation/plot4.py
+++ V353-Relaxation/plot4.py
@@ -4,21 +4,21 @@ from scipy.optimize import curve_fit
 
 w, A, phi = np.genfromtxt('amplitude.txt', unpack=True)
 
-def f(w, d):
-   return 1/(np.sqrt(1 + d**2  * w**2))
+def f(phi, d):
+   return np.arctan(d*phi)
 
 x_plot = np.linspace(10, 5000, 5000)
-params, covariance_matrix = curve_fit(f, w, A/3.6)
+params, covariance_matrix = curve_fit(f, w, phi)
 errors = np.sqrt(np.diag(covariance_matrix))
 plt.plot(x_plot, f(x_plot, *params), 'k-', label='Anpassungsfunktion', linewidth=0.5)
 print(params)
 print(np.sqrt(np.diag(covariance_matrix)))
 plt.xscale('log')
 plt.gcf().subplots_adjust(bottom=0.18)
-plt.plot(w , A/3.6, 'r.', label='Messwerte', Markersize=4)
+plt.plot(w , phi, 'r.', label='Messwerte', Markersize=4)
 plt.title('Spannungsamplitude zu dem Zeitpunkt t')
 plt.legend()
 plt.grid()
 plt.xlabel(r'$w$/Hz')
 plt.ylabel(r'$A/$V')
-plt.savefig('build/plot2.pdf')
+plt.savefig('build/plot3.pdf')
