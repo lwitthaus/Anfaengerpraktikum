a209191bb701de232757ec90116b6d8006e00d6f
Christopher Krause <christopher2.krause@tu-dortmund.de>
2017-12-07 11:58:08 +0100

    miep

---
+++ V353-Relaxation/plot1.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
+from uncertainties import ufloat
+
+t, U = np.genfromtxt('entladung.txt', unpack=True)
+
+def f(x, c, b):
+   return c * x + b
+
+x_plot = np.linspace(0, 5)
+params, covariance_matrix = curve_fit(f, t, np.log(U))
+errors = np.sqrt(np.diag(covariance_matrix))
+plt.plot(x_plot, f(x_plot, *params), 'k-', label='Anpassungsfunktion', linewidth=0.5)
+print(params)
+print(np.sqrt(np.diag(covariance_matrix)))
+c = ufloat(params[0], errors[0])
+print('c = ', c)
+print(-1/c)
+plt.gcf().subplots_adjust(bottom=0.18)
+plt.plot(t ,np.log(U), 'r.', label='Spannung zum Zeitpunkt t', Markersize=4)
+plt.title('Kondensatorspannung zu dem Zeitpunkt t')
+plt.legend()
+plt.grid()
+plt.xlabel(r'$t/ \left( 10^{-3} \mathrm{s} \right)$')
+plt.ylabel(r'$\ln{\frac{U_C}{U_0}}$')
+plt.savefig('build/plot1.pdf')
