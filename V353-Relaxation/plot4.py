1def8b46842f192632d908063f9068b3d322f889
Christopher Krause <christopher2.krause@tu-dortmund.de>
2017-12-07 16:20:38 +0100

    Diskussion

--- V353-Relaxation/plot4.py
+++ V353-Relaxation/plot4.py
@@ -5,9 +5,9 @@ from scipy.optimize import curve_fit
 w, A, phi = np.genfromtxt('amplitude.txt', unpack=True)
 
 def f(phi, d):
-   return np.arctan(d*phi)
+   return np.arctan(d*phi) * 180/np.pi
 
-x_plot = np.linspace(0, 5000)
+x_plot = np.linspace(10, 5000, 5000)
 params, covariance_matrix = curve_fit(f, w, phi)
 errors = np.sqrt(np.diag(covariance_matrix))
 plt.plot(x_plot, f(x_plot, *params), 'k-', label='Anpassungsfunktion', linewidth=0.5)
