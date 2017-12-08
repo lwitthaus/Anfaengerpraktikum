4c475fb783b1738b04bffa724f3af38dba47b7fb
Christopher Krause <christopher2.krause@tu-dortmund.de>
2017-12-08 10:03:52 +0100

    blub

--- V353-Relaxation/plot2.py
+++ V353-Relaxation/plot2.py
@@ -5,7 +5,7 @@ from scipy.optimize import curve_fit
 f, A, phi = np.genfromtxt('amplitude.txt', unpack=True)
 
 def fit(f, d):
-   return 1/(np.sqrt(1 + d**2  * (f*(2*np.pi))**2))
+   return 1/(np.sqrt(1 + d**2  * (f*2*np.pi)**2))
 
 x_plot = np.linspace(0, 35000, 5000)
 params, covariance_matrix = curve_fit(fit, f*(np.pi *2), A/3.6)
