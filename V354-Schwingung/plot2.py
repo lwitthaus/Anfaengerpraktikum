f2ef7b9a36fa10cb5da49f6b54c8be305f29e83b
Christopher Krause <christopher2.krause@tu-dortmund.de>
2017-12-14 14:20:14 +0100

    Auswertung

--- V354-Schwingung/plot2.py
+++ V354-Schwingung/plot2.py
@@ -4,28 +4,28 @@ from scipy.optimize import curve_fit
 from uncertainties import ufloat
 import uncertainties.unumpy as unp
 
-#R = ufloat(509.5, 0.1)
-#L = ufloat(10.11*10**(-3), 0.03*10**(-3))
-#C = ufloat(2.098*10**(-9), 0.006*10**(-9))
-#
-f1, U0, UC, a, U, phi = np.genfromtxt('phase.txt', unpack=True)
-#
-#def f(f1, A, B):
-#   return 1/unp.sqrt((1-L*C*(f1*2*np.pi)**2)**2 + (f1*2*np.pi)**2 * (R*C)**2)
-#
-#x_plot = np.linspace(0, 400, 400)
-#params, covariance_matrix = curve_fit(f, np.log(f1*2*np.pi), U)
-#errors = np.sqrt(np.diag(covariance_matrix))
-#plt.plot(x_plot, f(x_plot, *params), 'k-', label='Anpassungsfunktion', linewidth=0.5)
-#print(params)
+R = 509.5
+L = 10.11*10**(-3)
+C = 2.098*10**(-9)
+
+w, U0, UC, a, U, phi = np.genfromtxt('phase.txt', unpack=True)
+
+def f(w):
+       return 1/np.sqrt((1-L*C*(w*2*np.pi)**2)**2 + (w*2*np.pi)**2 * (R*C)**2)
+
+x_plot = np.linspace(1000, 100000, 1000)
+plt.plot((x_plot*2*np.pi), f(x_plot))
+
+
 #print(np.sqrt(np.diag(covariance_matrix)))
 #print('U_0 gleich ', params[0], ' +- ', errors[0])
 #print('my gleich ', params[1], ' +-' , errors[1])
 plt.gcf().subplots_adjust(bottom=0.18)
-plt.plot(np.log(f1*2*np.pi) , U, 'r.', label='Messwerte', Markersize=4)
+plt.plot((w*2*np.pi) , U, 'r.', label='Messwerte', Markersize=4)
+plt.xscale("log")
 plt.title('Spannungsamplituden in Abhängigkeit der Frequenz')
 plt.legend()
 plt.grid()
-plt.xlabel(r'$f/$s')
+plt.xlabel(r'$f/$Hz')
 plt.ylabel(r'$\frac{U_C}{U_0}$')
 plt.savefig('build/plot2.pdf')
