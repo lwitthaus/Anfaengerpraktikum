13f68da3c8c9098deb4eee4a76b5a4f3acaa84c3
Christopher Krause <christopher2.krause@tu-dortmund.de>
2017-12-20 09:08:09 +0100

    Korrektur

--- V353-Relaxation/plot1.py
+++ V353-Relaxation/plot1.py
@@ -8,7 +8,7 @@ t, U = np.genfromtxt('entladung.txt', unpack=True)
 def f(x, c, b):
    return c * x + b
 
-x_plot = np.linspace(0, 5)
+x_plot = np.linspace(0, 6)
 params, covariance_matrix = curve_fit(f, t, np.log(U))
 errors = np.sqrt(np.diag(covariance_matrix))
 plt.plot(x_plot, f(x_plot, *params), 'k-', label='Anpassungsfunktion', linewidth=0.5)
@@ -21,7 +21,8 @@ plt.gcf().subplots_adjust(bottom=0.18)
 plt.plot(t ,np.log(U), 'r.', label='Messwerte', Markersize=4)
 plt.title('Kondensatorspannung zu dem Zeitpunkt t')
 plt.legend()
+plt.xlim((0, 5.5))
 plt.grid()
-plt.xlabel(r'$t/ \left( 10^{-3} \mathrm{s} \right)$')
+plt.xlabel(r'$t/ms$')
 plt.ylabel(r'$\ln{\frac{U_C}{U_0}}$')
 plt.savefig('build/plot1.pdf')
--- V353-Relaxation/plot4.py
+++ V353-Relaxation/plot4.py
@@ -4,21 +4,22 @@ from scipy.optimize import curve_fit
 
 w, A, phi = np.genfromtxt('amplitude.txt', unpack=True)
 
-def f(phi, d):
-   return np.arctan(d*phi) * 180/np.pi
+def f(w, d):
+   return np.arctan(d*w*2*np.pi)*180/np.pi
 
 x_plot = np.linspace(10, 35000, 5000)
-params, covariance_matrix = curve_fit(f, w*2*np.pi, phi)
+params, covariance_matrix = curve_fit(f, w, phi)
 errors = np.sqrt(np.diag(covariance_matrix))
 plt.plot(x_plot, f(x_plot, *params), 'k-', label='Anpassungsfunktion', linewidth=0.5)
 print(params)
 print(np.sqrt(np.diag(covariance_matrix)))
 plt.xscale('log')
 plt.gcf().subplots_adjust(bottom=0.18)
-plt.plot(w*2*np.pi , phi, 'r.', label='Messwerte', Markersize=4)
+plt.plot(w , phi, 'r.', label='Messwerte', Markersize=4)
 plt.title('Phasenverschiebung in Abhängigkeit der Frequenz')
 plt.legend()
 plt.grid()
-plt.xlabel(r'$w$/Hz')
+plt.xlim((10, 2*10**4))
+plt.xlabel(r'$f$/Hz')
 plt.ylabel(r'$\varphi/$°')
 plt.savefig('build/plot4.pdf')
