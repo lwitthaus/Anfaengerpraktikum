9b0e1b1b4c30886de4c3a80089092727a1110580
Christopher Krause <christopher2.krause@tu-dortmund.de>
2017-12-21 11:34:53 +0100

    Auswertung

---
+++ V301-EMK/plot4.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
+from uncertainties import ufloat
+
+I, U = np.genfromtxt('rechteckspannung.txt', unpack=True)
+
+def f(I, a, b):
+   return a*I + b
+
+x_plot = np.linspace(0, 1.2, 400)
+params, covariance_matrix = curve_fit(f, I, U)
+errors = np.sqrt(np.diag(covariance_matrix))
+plt.plot(x_plot, f(x_plot, *params), 'k-', label='Anpassungsfunktion', linewidth=0.5)
+print(params)
+print(np.sqrt(np.diag(covariance_matrix)))
+plt.gcf().subplots_adjust(bottom=0.18)
+plt.plot(I , U, 'r.', label='Messwerte', Markersize=4)
+plt.title('Klemmspannung bei angelegter Rechteckspannung')
+plt.legend()
+plt.grid()
+plt.xlim((0, 1.1))
+plt.xlabel(r'$I/\mathrm{mA}$')
+plt.ylabel(r'$U/$V')
+plt.savefig('build/plot4.pdf')
