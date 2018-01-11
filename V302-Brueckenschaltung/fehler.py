0cb2988efff5e178b28d2edb6d6d0813fd164d3e
Christopher Krause <christopher2.krause@tu-dortmund.de>
2018-01-11 11:23:20 +0100

    Auswertung

--- V302-Brueckenschaltung/fehler.py
+++ V302-Brueckenschaltung/fehler.py
@@ -22,20 +22,116 @@ from uncertainties.umath import *
 #print(a)
 
 #zweiter Widerstand, Wert 12
-R_2 = ufloat(500, 1)
-R_v = ufloat(0.748, 0.004)
-R_x = R_2 * R_v
-print(R_x)
+#R_2 = ufloat(500, 1)
+#R_v = ufloat(0.748, 0.004)
+#R_x = R_2 * R_v
+#print(R_x)
+#
+#R_q = ufloat(1000, 2)
+#R_w = ufloat(0.357, 0.002)
+#R_y = R_q * R_w
+#print(R_y)
+#
+#R_e = ufloat(332, 1)
+#R_r = ufloat(1.128, 0.006)
+#R_z = R_e * R_r
+#print(R_z)
+#
+#a = np.std([374.0, 357.0, 374.5])
+#print(a)
 
-R_q = ufloat(1000, 2)
-R_w = ufloat(0.357, 0.002)
-R_y = R_q * R_w
-print(R_y)
 
-R_e = ufloat(332, 1)
-R_r = ufloat(1.128, 0.006)
-R_z = R_e * R_r
-print(R_z)
 
-a = np.std([374.0, 357.0, 374.5])
-print(a)
+
+#Kapazität Wert 1
+#C_q = ufloat(992*10**(-9), 2*10**(-9))
+#R_q = ufloat(0.667, 0.003)
+#C_1 = C_q * R_q
+#print(C_1)
+
+
+#C_w = ufloat(597*10**(-9), 1*10**(-9))
+#R_w = ufloat(1.110, 0.006)
+#C_2 = C_w * R_w
+#print(C_2)
+
+#C_e = ufloat(399*10**(-9), 1*10**(-9))
+#R_e = ufloat(1.660, 0.008)
+#C_3 = C_e * R_e
+#print(C_3)
+
+#a = np.std([6.62, 6.63, 6.62])
+#print(a)
+
+
+
+
+#Kapazität Wert 3
+#C_e = ufloat(399*10**(-9), 1*10**(-9))
+#R_e = ufloat(1.053,0.005)
+#C_1 = C_e * R_e
+#print(C_1)
+
+#C_w = ufloat(597*10**(-9), 1*10**(-9))
+#R_w = ufloat(0.704, 0.004)
+#C_2 = C_w * R_w
+#print(C_2)
+
+#C_q = ufloat(992*10**(-9), 2*10**(-9))
+#R_q = ufloat(0.422, 0.002)
+#C_3 = C_q * R_q
+#print(C_3)
+
+#a= np.std([4.201, 4.203, 4.186])
+#print(a)
+
+
+
+
+#Kapazität (Wert 8)
+#C_q = ufloat(992*10**(-9), 2*10**(-9))
+#R_q = ufloat(0.297, 0.001)
+#C_1 = C_q * R_q
+#R_a = ufloat(170, 5.1)
+#print(C_1)
+#
+#C_w = ufloat(597*10**(-9), 1*10**(-9))
+#R_w = ufloat(0.490, 0.002)
+#C_2 = C_w * R_w
+#print(C_2)
+#
+#C_e = ufloat(399*10**(-9), 1*10**(-9))
+#R_e = ufloat(0.736, 0.004)
+#C_3 = C_e * R_e
+#print(C_3)
+#
+#a = np.std([2.946, 2.925, 2.937])
+#print(a)
+
+
+#R_a = ufloat(170, 5.1)
+#R_q = ufloat(1/0.30, 0.02)
+#R_1 = R_a * R_q
+#print(R_1)
+#
+#R_b = ufloat(281, 8.43)
+#R_w = ufloat(1/0.49, 0.01)
+#R_2 = R_b * R_w
+#print(R_2)
+#
+#R_c = ufloat(422, 12.66)
+#R_e = ufloat(1/0.736, 0.007)
+#R_3 = R_c * R_e
+#print(R_3)
+#
+#a = np.std([667, 673, 673])
+#print(a)
+
+
+
+
+#Induktivität Wert 17
+L_q = ufloat(27.5*10**(-3), 0.06*10**(-3))
+R_q = ufloat(1.545, 0.008)
+L_1 = L_q * R_q
+print(L_1)
