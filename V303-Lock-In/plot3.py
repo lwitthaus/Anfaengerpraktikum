eb0d50cf402c214006f2720eab7d2cf6710ff463
Christopher Krause <christopher2.krause@tu-dortmund.de>
2018-01-18 11:17:38 +0100

    getting shit done

--- V303-Lock-In/plot3.py
+++ V303-Lock-In/plot3.py
@@ -3,22 +3,22 @@ import numpy as np
 from scipy.optimize import curve_fit
 from uncertainties import ufloat
 
-phi, U, Uv = np.genfromtxt('phase.txt', unpack=True)
+r, U = np.genfromtxt('diode.txt', unpack=True)
+#
+def f(r, A):
+   return A*1/(r)**2
 
-def f(phi, A, B, C):
-   return A* np.cos(phi*2*np.pi / 360 + B) + C
-
-x_plot = np.linspace(-10, 310, 400)
-params, covariance_matrix = curve_fit(f, phi, U)
+x_plot = np.linspace(2, 80, 400)
+params, covariance_matrix = curve_fit(f, r, U)
 errors = np.sqrt(np.diag(covariance_matrix))
 plt.plot(x_plot, f(x_plot, *params), 'k-', label='Anpassungsfunktion', linewidth=0.5)
 print(params)
 print(np.sqrt(np.diag(covariance_matrix)))
 plt.gcf().subplots_adjust(bottom=0.18)
-plt.plot(phi , U, 'r.', label='Messwerte', Markersize=4)
+plt.plot(r , U, 'r.', label='Messwerte', Markersize=4)
 plt.legend()
 plt.grid()
-plt.xlim((-10, 310))
-plt.xlabel(r'$\phi / $grad')
+plt.xlim((2, 80))
+plt.xlabel(r'$r / $cm')
 plt.ylabel(r'$U/$mV')
-plt.savefig('build/plot.pdf')
+plt.savefig('build/plot3.pdf')
