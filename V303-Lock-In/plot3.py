788dca535ba6f55601ae6e1a5f5256afd29694a7
Christopher Krause <christopher2.krause@tu-dortmund.de>
2018-01-18 12:17:13 +0100

    fucking merge that shit

--- V303-Lock-In/plot3.py
+++ V303-Lock-In/plot3.py
@@ -4,11 +4,11 @@ from scipy.optimize import curve_fit
 from uncertainties import ufloat
 
 r, U = np.genfromtxt('diode.txt', unpack=True)
-#
-def f(r, A):
-   return A*1/(r)**2
 
-x_plot = np.linspace(2, 80, 400)
+def f(r, A, B):
+   return A*1/r**B
+
+x_plot = np.linspace(1, 80, 400)
 params, covariance_matrix = curve_fit(f, r, U)
 errors = np.sqrt(np.diag(covariance_matrix))
 plt.plot(x_plot, f(x_plot, *params), 'k-', label='Anpassungsfunktion', linewidth=0.5)
@@ -18,7 +18,9 @@ plt.gcf().subplots_adjust(bottom=0.18)
 plt.plot(r , U, 'r.', label='Messwerte', Markersize=4)
 plt.legend()
 plt.grid()
-plt.xlim((2, 80))
+plt.xscale('log')
+plt.yscale('log')
+plt.xlim((1, 80))
 plt.xlabel(r'$r / $cm')
 plt.ylabel(r'$U/$mV')
 plt.savefig('build/plot3.pdf')
