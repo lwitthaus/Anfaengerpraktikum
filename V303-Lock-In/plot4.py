b5d05afd7684f57d2d55abce18639fc6397f19bd
Lucas Witthaus <lucas.witthaus@udo.edu>
2018-01-26 14:05:11 +0100

    graph

--- V303-Lock-In/plot4.py
+++ V303-Lock-In/plot4.py
@@ -5,10 +5,10 @@ from uncertainties import ufloat
 
 r, U = np.genfromtxt('diode.txt', unpack=True)
 
-def f(r, A, B, C):
-   return np.log(A/r**B) + C
+def f(r, A, B):
+   return A*r + B
 
-x_plot = np.linspace(1, 6, 400)
+x_plot = np.linspace(0.1, 4.5, 400)
 params, covariance_matrix = curve_fit(f, np.log(r), np.log(U))
 errors = np.sqrt(np.diag(covariance_matrix))
 plt.plot(x_plot, f(x_plot, *params), 'k-', label='Anpassungsfunktion', linewidth=0.5)
@@ -18,7 +18,7 @@ plt.gcf().subplots_adjust(bottom=0.18)
 plt.plot(np.log(r) , np.log(U), 'r.', label='Messwerte', Markersize=4)
 plt.legend()
 plt.grid()
-plt.xlim((1, 5))
-plt.xlabel(r'$r / $cm')
-plt.ylabel(r'$U/$mV')
+plt.xlim((1, 4.5))
+plt.xlabel(r'$ln(r)')
+plt.ylabel(r'$ln(U)')
 plt.savefig('build/plot4.pdf')
