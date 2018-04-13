b0a24249ba23480ae6e1e9a5d80ab9bdd3f42534
Lucas Witthaus <lucas.witthaus@udo.edu>
2018-04-13 11:00:39 +0200

    bla

--- V501,2- Elektronenstrahl/plot.py
+++ V501,2- Elektronenstrahl/plot.py
@@ -6,13 +6,13 @@ from uncertainties import ufloat
 
 I, D = np.genfromtxt('daten_bfeld_250.txt', unpack=True)
 L = 0.175
-N = 20
+N = 20*10**4
 R = 0.282
 
 def f(x, a, b):
    return a * x + b
 
-x_plot = np.linspace(-0.00001, 0.00018)
+x_plot = np.linspace(-0.1, 1.8)
 params, covariance_matrix = curve_fit(f, mu_0*8/np.sqrt(125)*N*I/R, D/(L**2+D**2))
 errors = np.sqrt(np.diag(covariance_matrix))
 plt.plot(x_plot, f(x_plot, *params), 'k-', label='Anpassungsfunktion', linewidth=0.5)
@@ -20,19 +20,18 @@ print(params)
 print(np.sqrt(np.diag(covariance_matrix)))
 plt.gcf().subplots_adjust(bottom=0.18)
 plt.plot(mu_0*8/np.sqrt(125)*N*I/R, D/(L**2+D**2), 'r.', label='Messwerte', Markersize=4)
-plt.title('Ausgleichsrechnung für Proportionalitätsfaktor')
 plt.legend()
 plt.grid()
-plt.xlim((-0.00001, 0.00018))
-plt.xlabel(r'$\mu_0\cdot\frac{8}{\sqrt{125}}\cdot\frac{NI}{R}$')
-plt.ylabel(r'$\frac{D}{L^2 + D^2}$')
+plt.xlim((-0.1, 1.8))
+plt.xlabel(r'$B = \mu_0\cdot\frac{8}{\sqrt{125}}\cdot\frac{NI}{R}\: / (10^{-4} \cdot T)$')
+plt.ylabel(r'$\frac{D}{L^2 + D^2}\: / \frac{1}{m}$')
 plt.savefig('build/plot.pdf')
 
 plt.clf()
 
 I, D = np.genfromtxt('daten_bfeld_400.txt', unpack=True)
 
-x_plot = np.linspace(-0.00001, 0.00018)
+x_plot = np.linspace(-0.1, 1.8)
 params, covariance_matrix = curve_fit(f, mu_0*8/np.sqrt(125)*N*I/R, D/(L**2+D**2))
 errors = np.sqrt(np.diag(covariance_matrix))
 plt.plot(x_plot, f(x_plot, *params), 'k-', label='Anpassungsfunktion', linewidth=0.5)
@@ -40,10 +39,9 @@ print(params)
 print(np.sqrt(np.diag(covariance_matrix)))
 plt.gcf().subplots_adjust(bottom=0.18)
 plt.plot(mu_0*8/np.sqrt(125)*N*I/R, D/(L**2+D**2), 'r.', label='Messwerte', Markersize=4)
-plt.title('Ausgleichsrechnung für Proportionalitätsfaktor')
 plt.legend()
 plt.grid()
-plt.xlim((-0.00001, 0.00018))
-plt.xlabel(r'$\mu_0\cdot\frac{8}{\sqrt{125}}\cdot\frac{NI}{R}$')
-plt.ylabel(r'$\frac{D}{L^2 + D^2}$')
+plt.xlim((-0.1, 1.8))
+plt.xlabel(r'$B = \mu_0\cdot\frac{8}{\sqrt{125}}\cdot\frac{NI}{R}\: / (10^{-4} \cdot T) $')
+plt.ylabel(r'$\frac{D}{L^2 + D^2}\: / \frac{1}{m}$')
 plt.savefig('build/plot2.pdf')
