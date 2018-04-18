255fd27edb6c1a3e3890bfb0caee40596977b02a
Christopher Krause <christopher2.krause@tu-dortmund.de>
2018-04-18 13:47:38 +0200

    Auswertung

--- V500-Photoeffekt/plotrot.py
+++ V500-Photoeffekt/plotrot.py
@@ -4,13 +4,13 @@ from scipy.constants import mu_0
 from scipy.optimize import curve_fit
 from uncertainties import ufloat
 
-U, I = np.genfromtxt('gruen.txt', unpack=True)
+U, I = np.genfromtxt('rot.txt', unpack=True)
 
 
 def f(x, a, b):
    return a * x + b
 
-x_plot = np.linspace(-1, 4)
+x_plot = np.linspace(-1, 7)
 params, covariance_matrix = curve_fit(f, U, I**(1/2))
 errors = np.sqrt(np.diag(covariance_matrix))
 plt.plot(x_plot, f(x_plot, *params), 'k-', label='Anpassungsfunktion', linewidth=0.5)
@@ -20,7 +20,7 @@ plt.gcf().subplots_adjust(bottom=0.18)
 plt.plot(U, I**(1/2), 'r.', label='Messwerte', Markersize=4)
 plt.legend()
 plt.grid()
-plt.xlim((-1, 4))
+plt.xlim((-1, 7))
 plt.ylabel(r'$\sqrt{I} / \mathrm{\sqrt{nA}}$')
 plt.xlabel(r'$U / V$')
-plt.savefig('build/plot.pdf')
+plt.savefig('build/plotrot.pdf')
