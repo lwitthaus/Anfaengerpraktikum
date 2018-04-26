8b2a044e1ec98aea530beba17922f442383cb39b
Christopher Krause <christopher2.krause@tu-dortmund.de>
2018-04-26 11:06:37 +0200

    some shit

--- V602-Roentgenemission/plotstrontium.py
+++ V602-Roentgenemission/plotstrontium.py
@@ -3,17 +3,17 @@ import numpy as np
 from scipy.optimize import curve_fit
 from uncertainties import ufloat
 
-w, U = np.genfromtxt('emission.txt', unpack=True)
+w, U = np.genfromtxt('stronzium.txt', unpack=True)
 
 
 
-x_plot = np.linspace(7, 55)
+x_plot = np.linspace(17, 25)
 plt.gcf().subplots_adjust(bottom=0.18)
 plt.plot(w, U, 'r.', label='Messwerte', Markersize=4)
 plt.legend()
 plt.grid()
-plt.xlim((7, 55))
-plt.ylim((30, 5000))
+plt.xlim((17, 25))
+plt.ylim((40, 205))
 plt.xlabel(r'$\Theta/$Grad')
 plt.ylabel(r'I')
-plt.savefig('build/plot.pdf')
+plt.savefig('build/plotstrontium.pdf')
--- V602-Roentgenemission/plotzirconium.py
+++ V602-Roentgenemission/plotzirconium.py
@@ -3,17 +3,17 @@ import numpy as np
 from scipy.optimize import curve_fit
 from uncertainties import ufloat
 
-w, U = np.genfromtxt('emission.txt', unpack=True)
+w, U = np.genfromtxt('zirkonium.txt', unpack=True)
 
 
 
-x_plot = np.linspace(7, 55)
+x_plot = np.linspace(15, 24)
 plt.gcf().subplots_adjust(bottom=0.18)
 plt.plot(w, U, 'r.', label='Messwerte', Markersize=4)
 plt.legend()
 plt.grid()
-plt.xlim((7, 55))
-plt.ylim((30, 5000))
+plt.xlim((15, 24))
+plt.ylim((100, 305))
 plt.xlabel(r'$\Theta/$Grad')
 plt.ylabel(r'I')
-plt.savefig('build/plot.pdf')
+plt.savefig('build/plotzirconium.pdf')
