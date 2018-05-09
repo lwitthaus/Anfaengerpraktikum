0aa34f03c41c281ddd77128e95e3caf069731bcd
Christopher Krause <christopher2.krause@tu-dortmund.de>
2018-05-09 14:23:24 +0200

    Auswertung

--- V606-Suszeptibilitaet/plot.py
+++ V606-Suszeptibilitaet/plot.py
@@ -1,21 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.constants import mu_0
+from scipy.optimize import curve_fit
+from uncertainties import ufloat
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
-
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
-
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
-
-# in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+f, U = np.genfromtxt('spannung.txt', unpack=True)
+plt.gcf().subplots_adjust(bottom=0.18)
+plt.plot(f, U, 'r.', label='Messwerte $U_B=200$V', Markersize=4)
+plt.grid()
+#plt.xlim((-0.1, 1.8))
+plt.xlabel(r'$f/$kHz')
+plt.ylabel(r'$U/$mV')
 plt.savefig('build/plot.pdf')
