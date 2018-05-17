869e5fd2474d0e5f1ad7f3457f8101719cc6ab6e
Christopher Krause <christopher2.krause@tu-dortmund.de>
2018-05-17 12:09:51 +0200

    miep

--- V704-Strahlungsabsorption/plot.py
+++ V704-Strahlungsabsorption/plot.py
@@ -1,21 +1,30 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.constants import mu_0
+from scipy.optimize import curve_fit
+from uncertainties import ufloat
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+D, A = np.genfromtxt('gammazahlen.txt', unpack=True)
+#F =[1.7,]
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
+def f(x, a, b):
+   return a * x + b
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
 
-# in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+x_plot = np.linspace(1, 21)
+params, covariance_matrix = curve_fit(f, D, np.log(A))
+errors = np.sqrt(np.diag(covariance_matrix))
+plt.plot(x_plot, f(x_plot, *params), 'k-', label='Anpassungsfunktion', linewidth=0.5)
+print(params)
+print(np.sqrt(np.diag(covariance_matrix)))
+#errA = np.sqrt(A)
+plt.errorbar(D, np.log(A), yerr= np.log(A), fmt = '.')
+plt.gcf().subplots_adjust(bottom=0.18)
+plt.plot(D, np.log(A), 'r.', label='Messwerte', Markersize=4)
+plt.legend()
+plt.grid()
+plt.xlim((1, 21))
+#plt.ylim((2, 20))
+plt.ylabel(r'$A/$s')
+plt.xlabel(r'$D/$mm')
 plt.savefig('build/plot.pdf')
