b52c53b800f999b00ce18b69b1739812a304cfa2
Christopher Krause <christopher2.krause@tu-dortmund.de>
2018-05-25 16:41:00 +0200

    Auswertung

--- V703-Geigerzaehler/rechnung.py
+++ V703-Geigerzaehler/rechnung.py
@@ -5,6 +5,19 @@ from uncertainties import ufloat
 import uncertainties.unumpy as unp
 
 U, N, I = np.genfromtxt('datenfit.txt', unpack=True)
-F = np.sqrt(N)
+F = I*60* 10**(-6)/(np.sqrt(N)*1.6*10**(-19))
 R = unp.uarray([N],[F])
-print(I*60* 10**(-6)/(R*1.6*10**(-19)))
+#H = np.array[0.01, 0.02, 0.02, 0.02, 0.03, 0.03, 0.04, 0.04, 0.04, 0.04, 0.04, 0.05, 0.05, 0.05, 0.05, 0.06, 0.06, 0.06, 0.07, 0.07, 0.07, 0.08, 0.08, 0.08, 0.08, 0.09, 0.09, 0.09, 0.10, 0.10, 0.10]
+#0.10, 0.11, 0.11, 0.11, 0.11, 0.11, 0.12, 0.12]
+
+Q = I*60* 10**(-6)/(N*1.6*10**(-19))
+K = np.sqrt(Q)
+
+plt.plot(U, Q, 'b.', label='Messwerte', Markersize=4)
+plt.errorbar(U, Q, yerr= K, fmt = '.')
+
+plt.legend()
+plt.grid()
+plt.ylabel(r'$Q/e$')
+plt.xlabel(r'$U/$V')
+plt.savefig('build/rechnung.pdf')
