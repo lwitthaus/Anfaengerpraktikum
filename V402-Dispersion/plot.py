17250731d4a3dbecf9dd46101b62a4788ca6857b
Christopher Krause <christopher2.krause@tu-dortmund.de>
2018-06-14 12:21:43 +0200

    hefejrgf

--- V402-Dispersion/plot.py
+++ V402-Dispersion/plot.py
@@ -6,15 +6,15 @@ from uncertainties import ufloat
 l = np.array([404.6 ,439.8 ,480.0 ,502.5 ,546.1 ,577.0 ,615.8 ,656.7])
 n = np.array([1.813, 1.805, 1.784, 1.781, 1.771, 1.764, 1.760, 1.750])
 
-#def f(A, B, C, l):
-#   return A + B/(l**2) + C/(l**4)
-#
-#x_plot = np.linspace(400, 700)
-#params, covariance_matrix = curve_fit(f, l, n**2)
-#errors = np.sqrt(np.diag(covariance_matrix))
-#plt.plot(x_plot, f(x_plot, *params), 'k-', label='Anpassungsfunktion', linewidth=0.5)
-#print(params)
-#print(np.sqrt(np.diag(covariance_matrix)))
+def f(A, B, l):
+   return A + B/(l**2)
+
+x_plot = np.linspace(400, 700)
+params, covariance_matrix = curve_fit(f, l, n**2)
+errors = np.sqrt(np.diag(covariance_matrix))
+plt.plot(x_plot, f(x_plot, *params), 'k-', label='Anpassungsfunktion', linewidth=0.5)
+print(params)
+print(np.sqrt(np.diag(covariance_matrix)))
 
 plt.gcf().subplots_adjust(bottom=0.18)
 plt.plot(l , n**2, 'r.', label='Messwerte', Markersize=4)
