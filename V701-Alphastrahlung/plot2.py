ea2d1465c547a3ea52044aa719216d347cf161b6
Christopher Krause <christopher2.krause@tu-dortmund.de>
2018-06-21 13:20:39 +0200

    jhrefjehvfb

--- V701-Alphastrahlung/plot2.py
+++ V701-Alphastrahlung/plot2.py
@@ -5,28 +5,31 @@ from scipy.optimize import curve_fit
 from uncertainties import ufloat
 
 #p, N, c = np.genfromtxt('abstand1.txt', unpack=True)
-R =[24.80, 23.14, 22.60, 21.53, 20.39, 20.12, 18.42, 17.66, 16.59, 15.71, 14.76, 13.68, 12.70, 11.96, 10.88, 9.84, 8.83, 8.44]
-N = [81389, 81648, 79218, 78200, 78330, 77252, 76070, 74155, 73737, 72061, 70654, 68438, 65735, 64206, 57280, 49102, 40228, 26130]
+x =[0, 0.11, 0.23, 0.34, 0.45, 0.57, 0.68, 0.79, 0.91, 1.02, 1.14, 1.25, 1.36, 1.48, 1.59, 1.70, 1.82, 1.93, 2.04, 2.16, 2.27]
+N = np.array([81389, 81648, 79218, 78200, 78330, 77252, 76070, 74155, 73737, 72061, 70654, 68438, 65735, 64206, 57280, 49102, 40228, 26130, 17869, 9083, 3592])
 
 
-#def f(x, a, b):
-#   return a * x + b
+y =[1.70, 1.82, 1.93, 2.04]
+M = np.array([49102, 40228, 26130, 17869])
+
+def f(y, a, b):
+   return a * y + b
 
 
-#x_plot = np.linspace(290, 710)
-#params, covariance_matrix = curve_fit(f, U, N/60)
-#errors = np.sqrt(np.diag(covariance_matrix))
-#plt.plot(x_plot, f(x_plot, *params), 'k-', label='Anpassungsfunktion', linewidth=0.5)
-#print(params)
-#print(np.sqrt(np.diag(covariance_matrix)))
+x_plot = np.linspace(0, 2.3)
+params, covariance_matrix = curve_fit(f, y, M)
+errors = np.sqrt(np.diag(covariance_matrix))
+plt.plot(x_plot, f(x_plot, *params), 'k-', label='Anpassungsfunktion', linewidth=0.5)
+print(params)
+print(np.sqrt(np.diag(covariance_matrix)))
 
 plt.gcf().subplots_adjust(bottom=0.18)
 #plt.plot(U, N, 'r.', label='Messwerte', Markersize=4)
 plt.legend()
 plt.grid()
 #plt.xlim((290, 710))
-plt.plot(R, N, 'r.', label='Messwerte', Markersize=4)
-#plt.ylim((-1000, 30000))
-#plt.ylabel(r'$N / \frac{1}{s}$')
-#plt.xlabel(r'$U/$V')
+plt.plot(x, N, 'r.', label='Messwerte', Markersize=4)
+plt.ylim((0, 85000))
+plt.ylabel(r'$N$')
+plt.xlabel(r'$x/$cm')
 plt.savefig('build/plot2.pdf')
--- V701-Alphastrahlung/plot3.py
+++ V701-Alphastrahlung/plot3.py
@@ -5,29 +5,31 @@ from scipy.optimize import curve_fit
 from uncertainties import ufloat
 
 #p, N, c = np.genfromtxt('abstand1.txt', unpack=True)
-R =[24.80, 23.97, 23.33, 22.60, 21.97, 20.56, 19.18, 18.08, 17.17, 16.59, 15.79, 15.23, 14.52, 13.75, 13.60, 12.55, 11.82, 11.03, 10.46, 9.77, 9.50]
-N= [133097, 132258, 132252, 131151, 129489, 127602, 124606, 119948, 117771, 115922, 115965, 114158, 112766, 110443, 109593, 106939, 102803, 98282, 94237, 89703, 85338]
+x =[0, 0.08, 0.17, 0.25, 0.34, 0.42, 0.50, 0.59, 0.67, 0.76, 0.84, 0.92, 1.01, 1.09, 1.17, 1.26, 1.34, 1.43, 1.51, 1.59, 1.68]
+N= np.array([133097, 132258, 132252, 131151, 129489, 127602, 124606, 119948, 117771, 115922, 115965, 114158, 112766, 110443, 109593, 106939, 102803, 98282, 94237, 89703, 85338])
+
+y = [1.26, 1.34, 1.43, 1.51, 1.59, 1.68]
+M = np.array([106939, 102803, 98282, 94237, 89703, 85338])
+
+def f(y, a, b):
+   return a * y + b
 
 
-
-#def f(x, a, b):
-#   return a * x + b
-
-
-#x_plot = np.linspace(290, 710)
-#params, covariance_matrix = curve_fit(f, U, N/60)
-#errors = np.sqrt(np.diag(covariance_matrix))
-#plt.plot(x_plot, f(x_plot, *params), 'k-', label='Anpassungsfunktion', linewidth=0.5)
-#print(params)
-#print(np.sqrt(np.diag(covariance_matrix)))
+x_plot = np.linspace(0, 1.8)
+params, covariance_matrix = curve_fit(f, y, M)
+errors = np.sqrt(np.diag(covariance_matrix))
+plt.plot(x_plot, f(x_plot, *params), 'k-', label='Anpassungsfunktion', linewidth=0.5)
+print(params)
+print(np.sqrt(np.diag(covariance_matrix)))
 
 plt.gcf().subplots_adjust(bottom=0.18)
 #plt.plot(U, N, 'r.', label='Messwerte', Markersize=4)
 plt.legend()
 plt.grid()
 #plt.xlim((290, 710))
-plt.plot(R, N, 'r.', label='Messwerte', Markersize=4)
-#plt.ylim((-1000, 30000))
-#plt.ylabel(r'$N / \frac{1}{s}$')
-#plt.xlabel(r'$U/$V')
+plt.plot(x, N, 'r.', label='Messwerte', Markersize=4)
+plt.ylim((80000, 140000))
+plt.xlim((-0.01, 1.8))
+plt.ylabel(r'$N$')
+plt.xlabel(r'$x/$cm')
 plt.savefig('build/plot3.pdf')
