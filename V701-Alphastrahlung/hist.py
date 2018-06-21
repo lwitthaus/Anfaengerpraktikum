fa377702ede4562d450b2fa07f3fa0b56a2ba2de
Lucas Witthaus <lucas.witthaus@udo.edu>
2018-06-21 13:20:32 +0200

    plots

--- V701-Alphastrahlung/hist.py
+++ V701-Alphastrahlung/hist.py
@@ -3,8 +3,21 @@ import numpy as np
 from scipy.constants import mu_0
 from scipy.optimize import curve_fit
 from uncertainties import ufloat
+import math
 
 N = np.genfromtxt('statistik.txt', unpack=True)
 
-plt.hist(N, 12, normed=5, facecolor='blue', alpha=0.75)
+#print(np.mean(N))
+
+q = np.mean(N)
+r = np.mean(N**2)
+print(q)
+print(np.std(N))
+print(r-q**2)
+
+
+g = np.random.normal(np.mean(N), np.std(N), 10000)
+p = np.random.poisson(np.mean(N), 10000)
+plt.hist([g,N,p], 15, label=['Gauß', 'Messwerte', 'Poisson'], alpha=1, normed=1)
+plt.legend()
 plt.savefig('build/hist.pdf')
---
+++ V701-Alphastrahlung/plot4.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.constants import mu_0
+from scipy.optimize import curve_fit
+from uncertainties import ufloat
+
+x, E = np.genfromtxt('Energie.txt', unpack=True)
+
+def f(x, a, b):
+   return a * x + b
+
+
+x_plot = np.linspace(0,2)
+params, covariance_matrix = curve_fit(f, x, E)
+errors = np.sqrt(np.diag(covariance_matrix))
+plt.plot(x_plot, f(x_plot, *params), 'k-', label='Anpassungsfunktion', linewidth=0.5)
+print(params)
+print(np.sqrt(np.diag(covariance_matrix)))
+
+plt.plot(x, E, 'r.', label='Messwerte', Markersize=4)
+plt.gcf().subplots_adjust(bottom=0.18)
+plt.legend()
+plt.grid()
+plt.xlim((0,2))
+plt.ylabel(r'$x/$cm')
+plt.xlabel(r'$E/$MeV')
+plt.savefig('build/plot4.pdf')
