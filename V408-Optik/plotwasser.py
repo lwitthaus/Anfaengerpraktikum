7651c9152c9088f05fcd310f8f38169a841c17b9
Christopher Krause <christopher2.krause@tu-dortmund.de>
2018-07-05 13:06:44 +0200

    Auswertung

--- V408-Optik/plotwasser.py
+++ V408-Optik/plotwasser.py
@@ -4,19 +4,9 @@ from scipy.constants import mu_0
 from scipy.optimize import curve_fit
 from uncertainties import ufloat
 
-g, b, B = np.genfromtxt('daten1.txt', unpack=True)
-#a = np.array([[12.5, 0]  [12.0, 0]  [11.5, 0] [13.5, 0]  [14.0, 0]  [14.5, 0]  [15.0, 0]  [15.5, 0]  [16.0, 0] [16.5, 0]])
-c = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-#b1 = (0, 56.8)
-#b2 = (0, 72.0)
-#b3 = (0, 97.1)
-#b4 = (0, 40.8)
-#b5 = (0, 36.6)
-#b6 = (0, 32.9)
-#b7 = (0, 30.8)
-#b8 = (0, 29.0)
-#b9 = (0, 27.9)
-#b10 = (0, 25.5)
+g, b = np.genfromtxt('datenwasser.txt', unpack=True)
+
+
 
 #print(1/(1/g + 1/b))
 def f(x, a, b):
@@ -31,18 +21,9 @@ x_plot = np.linspace(0, 17)
 plt.gcf().subplots_adjust(bottom=0.18)
 for i in range(0, 9, 1):
     plt.plot([0, g[i]], [b[i], 0], 'bx-')
-#plt.plot(g, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 'r.', label='Messwerte', Markersize=4)
-#plt.plot([0, 0, 0, 0, 0, 0, 0, 0, 0, 0], b, 'r.', Markersize=4)
 plt.legend()
 plt.grid()
 #plt.xlim((10, 11))
 plt.ylabel(r'$b / $cm')
 plt.xlabel(r'$g / $cm')
-plt.savefig('build/plot.pdf')
-
-
-
-u, v = np.genfromtxt('datenwasser.txt', unpack=True)
-
-print(b/g)
-print(B/2.8)
+plt.savefig('build/plot2.pdf')
