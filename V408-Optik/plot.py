7b1f7440a5d0734199058602f04b3149d061eed2
Christopher Krause <christopher2.krause@tu-dortmund.de>
2018-07-05 11:40:35 +0200

    dfhbfdj

--- V408-Optik/plot.py
+++ V408-Optik/plot.py
@@ -1,21 +1,46 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.constants import mu_0
+from scipy.optimize import curve_fit
+from uncertainties import ufloat
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+g, b, B = np.genfromtxt('daten1.txt', unpack=True)
+#a = np.array([[12.5, 0]  [12.0, 0]  [11.5, 0] [13.5, 0]  [14.0, 0]  [14.5, 0]  [15.0, 0]  [15.5, 0]  [16.0, 0] [16.5, 0]])
+c = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+b1 = (0, 56.8)
+b2 = (0, 72.0)
+b3 = (0, 97.1)
+b4 = (0, 40.8)
+b5 = (0, 36.6)
+b6 = (0, 32.9)
+b7 = (0, 30.8)
+b8 = (0, 29.0)
+b9 = (0, 27.9)
+b10 = (0, 25.5)
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
+#print(1/(1/g + 1/b))
+def f(x, a, b):
+   return a * x + b
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
-
-# in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+x_plot = np.linspace(0, 17)
+#params, covariance_matrix = curve_fit(f, 12.5, 56.8)
+#errors = np.sqrt(np.diag(covariance_matrix))
+#plt.plot(x_plot, f(x_plot, *params), 'k-', label='Anpassungsfunktion', linewidth=0.5)
+#print(params)
+#print(np.sqrt(np.diag(covariance_matrix)))
+plt.gcf().subplots_adjust(bottom=0.18)
+for i in range(0, 9, 1):
+    plt.plot([0, g[i]], [b[i], 0], 'bx-')
+#plt.plot(g, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 'r.', label='Messwerte', Markersize=4)
+#plt.plot([0, 0, 0, 0, 0, 0, 0, 0, 0, 0], b, 'r.', Markersize=4)
+plt.legend()
+plt.grid()
+#plt.xlim((10, 11))
+plt.ylabel(r'$b / $cm')
+plt.xlabel(r'$g / $cm')
 plt.savefig('build/plot.pdf')
+
+
+
+u, v = np.genfromtxt('datenwasser.txt', unpack=True)
+print(1/(1/u + 1/v))
